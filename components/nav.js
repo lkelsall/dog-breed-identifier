aa996f943749792fef04cdaa3471441704c1a5f9
Benjamin Hood <benhood618@hotmail.com>
2021-07-28 12:28:43 +0100

    added photo and history buttons

--- components/nav.js
+++ components/nav.js
@@ -1,18 +1,31 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React from 'react';
+import { StyleSheet, View, Pressable, Text } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    height: "10%",
-    width: "100%",
-    backgroundColor: "blue",
+    height: '10%',
+    width: '100%',
+    backgroundColor: 'blue',
+  },
+  historyButton: {
+    alignSelf: 'flex-start',
+    backgroundColor: 'green',
+  },
+  photoButton: {
+    alignSelf: 'center',
+    backgroundColor: 'red',
   },
 });
 
 const Nav = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Pressable style={styles.historyButton}>
+        <Text>History</Text>
+      </Pressable>
+      <Pressable style={styles.photoButton}>
+        <Text>Take photo</Text>
+      </Pressable>
     </View>
   );
 };
