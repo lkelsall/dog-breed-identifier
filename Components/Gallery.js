b356f94e3be428439bf98c68682b6b5be726dd38
Sam-Jaya <samjfroude@gmail.com>
2021-07-29 14:42:51 +0100

    make changes to gallery component

--- Components/Gallery.js
+++ Components/Gallery.js
@@ -21,48 +21,10 @@ const Gallery = () => {
     });
   }, []); //this represents card images being saved to Async storage on phone-key value storage system
 
-  // const [dogPictures] = useState([
-  //   {
-  //     dogImage: require('../dog-images/dalmation.jpg'),
-  //     characteristic1: 'Happy nature',
-  //     createdAt: '27-07-2021-15-10',
-  //     breed: 'dalmation',
-  //   },
-  //   {
-  //     dogImage: require('../dog-images/husky.jpeg'),
-  //     characteristic1: 'Happy nature',
-  //     createdAt: '27-07-2021-15-15',
-  //     breed: 'dalmation',
-  //   },
-  //   {
-  //     dogImage: require('../dog-images/labrador.jpg'),
-  //     characteristic1: 'Happy nature',
-  //     createdAt: '27-07-2021-15-20',
-  //     breed: 'dalmation',
-  //   },
-  //   {
-  //     dogImage: require('../dog-images/pug.jpg'),
-  //     characteristic1: 'Happy nature',
-  //     createdAt: '27-07-2021-15-30',
-  //     breed: 'dalmation',
-  //   },
-  //   {
-  //     dogImage: require('../dog-images/puppy.jpg'),
-  //     characteristic1: 'Happy nature',
-  //     createdAt: '27-07-2021-15-40',
-  //     breed: 'dalmation',
-  //   },
-  // ]);
-  // const listDogs = () => {
-  //   return dogPictures.map((dog) => {
   return (
     <View>
       <Text>Its working</Text>
     </View>
   );
-  //   });
-  // };
-
-  // return <View>{listDogs()}</View>;
 };
 export default Gallery;
