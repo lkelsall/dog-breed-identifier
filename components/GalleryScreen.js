901c18c5d4afe2a54ebfcb7753a0d00ccc3a0813
Benjamin Hood <benhood618@hotmail.com>
2021-08-02 14:30:38 +0100

    Gallery screen

--- components/GalleryScreen.js
+++ components/GalleryScreen.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import PropTypes from 'prop-types';
+import { View, Image } from 'react-native';
+import { useState, useEffect } from 'react';
+import { readDirectory } from '../utils/storage-utils';
 
-const GalleryScreen = ({ route }) => {
-  const { dogFileStrings } = route.params;
-  console.log(dogFileStrings, 'in the gallery');
+const GalleryScreen = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [allDogs, setAllDogs] = useState([]);
+
+  useEffect(() => {
+    readDirectory()
+      .then((dogArray) => {
+        console.log(dogArray, 'In the useEffect');
+        setAllDogs(dogArray);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  if (isLoading) {
+    console.log('Loading');
+  }
   return (
     <View>
-      <Text>In the gallery</Text>
+      {allDogs.map((imageUri) => {
+        return <Image source={{ uri: imageUri }} key={imageUri} />;
+      })}
     </View>
   );
-};
-GalleryScreen.propTypes = {
-  route: PropTypes.object,
 };
 
 export default GalleryScreen;
