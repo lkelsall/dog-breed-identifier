0a968c7e3fd3a6e7de89e88aac9af903aba4ae02
Sam-Jaya <samjfroude@gmail.com>
2021-08-02 19:11:42 +0100

    update GalleryScreen to re-render each time new phot added

--- components/GalleryScreen.js
+++ components/GalleryScreen.js
@@ -3,11 +3,14 @@ import { View, Image, StyleSheet, Text } from 'react-native';
 import { useState, useEffect } from 'react';
 import { readDirectory } from '../utils/storage-utils';
 import * as FileSystem from 'expo-file-system';
+import { useIsFocused } from '@react-navigation/native';
 
 const GalleryScreen = () => {
+  const isFocused = useIsFocused();
   const [isLoading, setIsLoading] = useState(true);
 
   const [allDogs, setAllDogs] = useState([]);
+
   const photoDir = FileSystem.documentDirectory + 'dogs/';
 
   useEffect(() => {
@@ -19,12 +22,13 @@ const GalleryScreen = () => {
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [isFocused]);
   if (isLoading) {
     return <Text>...loading</Text>;
   }
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Gallery</Text>
       {allDogs.map((imageUri) => {
         return (
           <Image
@@ -43,8 +47,17 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'space-evenly',
-    alignContent: 'center',
+    alignContent: 'flex-start',
     flex: 1,
+    paddingTop: 60,
+  },
+  title: {
+    flex: 1,
+    color: 'white',
+    flexGrow: 1,
+    position: 'absolute',
+    paddingTop: 45,
+    paddingBottom: 20,
   },
 });
 
