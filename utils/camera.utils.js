ed25344b02a1c4e8af3101636f79ad8baa717059
lkelsall <kelsall.lewis@gmail.com>
2021-08-03 08:35:59 +0100

    Resize image in snap, and encode the small image in base64

--- utils/camera.utils.js
+++ utils/camera.utils.js
@@ -1,8 +1,22 @@
 import { storeDog } from './storage-utils/';
+import * as ImageManipulator from 'expo-image-manipulator';
+import * as FileSystem from 'expo-file-system';
 
 exports.snap = async (camera) => {
   let photo = await camera.takePictureAsync();
+  const smallPhoto = await ImageManipulator.manipulateAsync(photo.uri, [
+    { resize: { width: 200 } },
+  ]);
+
+  let options = { encoding: FileSystem.EncodingType.Base64 };
+  const base64image = await FileSystem.readAsStringAsync(
+    smallPhoto.uri,
+    options
+  );
+
+  base64image;
+
   let dogUri = await storeDog(photo.uri);
-  console.log(dogUri);
+
   return dogUri;
 };
