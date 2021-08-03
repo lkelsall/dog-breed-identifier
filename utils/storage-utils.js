fbf99e7a7ce6458a4e64f9c455679870f21f0892
lkelsall <kelsall.lewis@gmail.com>
2021-08-03 11:56:49 +0100

    Implement dog object file storage function

--- utils/storage-utils.js
+++ utils/storage-utils.js
@@ -1,8 +1,9 @@
 import * as FileSystem from 'expo-file-system';
 
-const photoDir = FileSystem.documentDirectory + 'dogs/';
+const dogsDir = FileSystem.documentDirectory + 'dogs/';
+const photoDir = FileSystem.documentDirectory + 'dogs/pics/';
 
-export const storeDog = async (uri) => {
+export const storeDogPic = async (uri) => {
   const newUri = `${photoDir}/${Date.now()}.jpg`;
   const directoryInfo = await FileSystem.getInfoAsync(photoDir);
   if (!directoryInfo.exists) {
@@ -12,5 +13,15 @@ export const storeDog = async (uri) => {
     from: uri,
     to: newUri,
   });
+  return newUri;
+};
+
+export const storeDogObj = async (dogObj) => {
+  const newUri = `${dogsDir}/${Date.now()}.txt`;
+  const directoryInfo = await FileSystem.getInfoAsync(dogsDir);
+  if (!directoryInfo.exist) {
+    await FileSystem.makeDirectoryAsync(dogsDir, { intermediates: true });
+  }
+  await FileSystem.writeAsStringAsync(newUri, dogObj);
   return newUri;
 };
