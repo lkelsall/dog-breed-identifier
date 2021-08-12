a1a5126762bb6b4ad8e6647b1ed78facbf09b79f
lkelsall <kelsall.lewis@gmail.com>
2021-08-12 15:26:05 +0100

    Remove some unused files

--- utils/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const getDogInfo = async (breed) => {
-  const { data } = await axios.get(
-    `https://dog-identifier-api.herokuapp.com/api/breeds/${breed}`
-  );
-
-  return data;
-};
