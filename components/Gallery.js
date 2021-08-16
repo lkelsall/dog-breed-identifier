4d0bc3aac47e7a4dd8600e522e6bf4a7f225db76
lkelsall <kelsall.lewis@gmail.com>
2021-08-16 16:45:43 +0100

    Re-style Gallery to accommodate smaller screens

--- components/Gallery.js
+++ components/Gallery.js
@@ -54,40 +54,24 @@ const Gallery = ({ setCurrentDog }) => {
   );
 };
 const styles = StyleSheet.create({
-  contentContainer: {
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: '#008080',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    padding: 10,
-  },
-  title: {
-    flex: 1,
-    color: 'white',
-    flexGrow: 1,
-    position: 'absolute',
-    paddingTop: 45,
-    paddingBottom: 20,
-  },
   scrollView: {
     height: '100%',
-    width: '98%',
-    margin: 20,
-    alignSelf: 'center',
-    padding: 20,
-    borderWidth: 5,
-    borderRadius: 5,
-    borderColor: 'black',
+    width: '100%',
+    padding: '5%',
     backgroundColor: '#008080',
   },
+  contentContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly',
+  },
   image: {
-    width: 105,
-    height: 105,
-    margin: 3,
+    width: 100,
+    height: 100,
     borderRadius: 5,
     borderWidth: 1,
-    borderColor: '#ffebcd',
+    borderColor: 'white',
+    marginVertical: '5%',
   },
 });
 
